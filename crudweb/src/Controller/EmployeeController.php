59dccf5d8375e45b3628015af8f38afdb5b0616e
Nguyen Dinh Tran Long <longndt226@gmail.com>
2022-11-19 10:05:09 +0700

    crud

---
+++ crudweb/src/Controller/EmployeeController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Employee;
+use App\Form\EmployeeType;
+use App\Repository\EmployeeRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EmployeeController extends AbstractController
+{
+    #[Route('/', name: 'app_employee_index', methods: ['GET'])]
+    public function index(EmployeeRepository $employeeRepository): Response
+    {
+        return $this->render('employee/index.html.twig', [
+            'employees' => $employeeRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_employee_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EmployeeRepository $employeeRepository): Response
+    {
+        $employee = new Employee();
+        $form = $this->createForm(EmployeeType::class, $employee);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $employeeRepository->save($employee, true);
+
+            return $this->redirectToRoute('app_employee_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('employee/new.html.twig', [
+            'employee' => $employee,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_employee_show', methods: ['GET'])]
+    public function show(Employee $employee): Response
+    {
+        return $this->render('employee/show.html.twig', [
+            'employee' => $employee,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_employee_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Employee $employee, EmployeeRepository $employeeRepository): Response
+    {
+        $form = $this->createForm(EmployeeType::class, $employee);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $employeeRepository->save($employee, true);
+
+            return $this->redirectToRoute('app_employee_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('employee/edit.html.twig', [
+            'employee' => $employee,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_employee_delete', methods: ['POST'])]
+    public function delete(Request $request, Employee $employee, EmployeeRepository $employeeRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$employee->getId(), $request->request->get('_token'))) {
+            $employeeRepository->remove($employee, true);
+        }
+
+        return $this->redirectToRoute('app_employee_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ crudweb/src/Form/EmployeeType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Employee;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EmployeeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('birthday')
+            ->add('mobile')
+            ->add('salary')
+            ->add('image')
+            ->add('experiencedyear')
+            ->add('married')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Employee::class,
+        ]);
+    }
+}
