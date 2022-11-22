ac3b28dc79893f9bd0af59def405e1afa309b8fd
Nguyen Dinh Tran Long <longndt226@gmail.com>
2022-11-22 10:01:59 +0700

    api

--- BlogRestAPI/src/Controller/BlogController.php
+++ BlogRestAPI/src/Controller/BlogController.php
@@ -12,27 +12,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BlogController extends AbstractController
 {
-    private $serializer;
+    private $blogRepository;
     private $registry;
-    public function __construct(SerializerInterface $serializer, ManagerRegistry $managerRegistry)
+    public function __construct(BlogRepository $blogRepository, ManagerRegistry $managerRegistry)
     {
-        $this->serializer = $serializer;
+        $this->blogRepository = $blogRepository;
         $this->registry = $managerRegistry;
     }
 
     #[Route('/', methods: ['GET'], name: 'view_all_blog_api')]
-    public function viewAllBlog(BlogRepository $blogRepository)
+    public function viewAllBlog(SerializerInterface $serializer)
     {
         //1) lấy dữ liệu từ DB và lưu vào array
         //SQL query tương ứng: "SELECT * FROM Blog"
         //Cách 1: dùng getDoctrine() và không cần khai báo parameter (tham số)
         //$blogs = $this->getDoctrine()->getRepository(Blog::class)->findAll();
         //Cách 2: dùng Repository, bắt buộc phải khai báo tham số (recommended)
-        $blogs = $blogRepository->findAll();
+        $blogs = $this->blogRepository->findAll();
 
         // 2) chuyển đổi array thành api (json hoặc xml)
         //$api = $serializer->serialize($blogs, 'xml');
-        $api = $this->serializer->serialize($blogs, 'json');
+        $api = $serializer->serialize($blogs, 'json');
 
         //3) trả về api cho front-end
         return new Response(
@@ -46,10 +46,10 @@ class BlogController extends AbstractController
     }
 
     #[Route('/{id}', methods: ['GET'], name: 'view_blog_by_id')]
-    public function viewBlogById($id, BlogRepository $blogRepository)
+    public function viewBlogById($id, SerializerInterface $serializer)
     {
         //SQL: SELECT * FROM Blog WHERE id = '$id'
-        $blog = $blogRepository->find($id);
+        $blog = $this->blogRepository->find($id);
         //check if blog is null return status code: 404 - Not Found
         if ($blog == null) {
             $error = "<center><h1 style='color: red;'><i><u>Blog not found !</u></i></h1></center>";
@@ -62,7 +62,7 @@ class BlogController extends AbstractController
             );
         }
 
-        $api = $this->serializer->serialize($blog, 'xml');
+        $api = $serializer->serialize($blog, 'xml');
         return new Response(
             $api,
             Response::HTTP_OK,
@@ -76,9 +76,9 @@ class BlogController extends AbstractController
 
     #[Route('/{id}', methods: 'DELETE', name: 'delete_blog')]
     //SQL: DELETE FROM Blog WHERE id = '$id'
-    public function deleteBlog($id, BlogRepository $blogRepository)
+    public function deleteBlog($id)
     {
-        $blog = $blogRepository->find($id);
+        $blog = $this->blogRepository->find($id);
         if ($blog == null) {
             $error = "<center><h1 style='color: red;'><i><u>Blog is not existed !</u></i></h1></center>";
             return new Response(
