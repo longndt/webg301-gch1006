8479be26d3368a0a1cdb89a480290a0ebd463409
Nguyen Dinh Tran Long <longndt226@gmail.com>
2022-11-24 09:13:03 +0700

    twig

--- twig/src/Controller/DemoController.php
+++ twig/src/Controller/DemoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,5 +15,30 @@ class DemoController extends AbstractController
         return $this->render('demo/index.html.twig', [
             'controller_name' => 'DemoController',
         ]);
+    }
+
+    #[Route('/worldcup', name: 'world_cup_2022')]
+    public function worldcup2022()
+    {
+        return $this->render('worldcup.html.twig');
+    }
+
+    #[Route('/', name: 'home')]
+    public function homepage()
+    {
+        $year = 2022;
+        $country = "Vietnam";
+        $datetime = DateTime::createFromFormat('d/m/Y H:i:s', '24/11/2022 09:09:45');
+        return $this->render(
+            'home.html.twig',
+            [
+                'namhientai' => $year,  //integer
+                'country' => $country,  //string
+                'grade' => 7.5,  //float
+                'gender' => 'F',  //character
+                'pass' => false, //boolean
+                'datetime' => $datetime //datetime
+            ]
+        );
     }
 }
